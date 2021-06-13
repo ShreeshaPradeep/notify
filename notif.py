66afa30efdcaba999baf50b19ed6f263f94fa451
shreesha <62246363+ShreeshaPradeep@users.noreply.github.com>
2021-06-13 17:56:21 +0530

    Update notif.py

--- notif.py
+++ notif.py
@@ -1,6 +1,8 @@
 import requests
 import time
+import time
 
+from plyer import notification
 
 dist = 266
 date = "13-06-2021"
@@ -28,13 +30,27 @@ def findAvailability():
         print("no Available Slots")
         return False
 
+
+def notifyMe(title,message):
+    notification.notify(
+        title = title,
+        message = message,
+        app_icon = "C:\\Users\\Shreesha\\Downloads\\Icons-Land-Medical-Equipment-Syringe-Full.ico",
+        timeout = 10,
+        )
+
 while(findAvailability()!=True):
-      time.sleep(2)
-      findAvailability()
+    if __name__ == '__main__':
+        notifyMe("oops :( !!!", "No available slots to book")
+    time.sleep(20)
+    findAvailability()
 
 while(findAvailability()==True):
-      time.sleep(10)
-      findAvailability()
+    if __name__ == '__main__':
+        notifyMe("HURRY HURRY.... !!!", "SLOTS ARE AVAILABLE TO BOOK")
+
+    time.sleep(10)
+    findAvailability()
 
 
 
