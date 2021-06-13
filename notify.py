1a4fd0603093bd4fc14c4488fcc13c0ef14158c3
shreesha <62246363+ShreeshaPradeep@users.noreply.github.com>
2021-06-13 17:35:25 +0530

    Add files via upload

---
+++ notify.py
@@ -0,0 +1,17 @@
+import time
+
+from plyer import notification
+
+
+def notifyMe(title,message):
+    notification.notify(
+        title = title,
+        message = message,
+        app_icon = "C:\\Users\\Shreesha\\Downloads\\Icons-Land-Medical-Equipment-Syringe-Full.ico",
+        timeout = 10,
+        )
+if __name__ == '__main__':
+    while(True):
+
+        notifyMe("Hey hurry up !!","Book your slots")
+        time.sleep(5)
